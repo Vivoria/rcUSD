2675912cbb4251f1df2dcf373dfc78d699acb28d
Nandy Bâ <nandyba@yahoo.fr>
2024-03-29 11:26:51 -0300

    feature: add addMinter function

--- src/RCUSD.sol
+++ src/RCUSD.sol
@@ -9,12 +9,15 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 contract RCUSD is ERC20, AccessControl, ERC20Permit {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor(address defaultAdmin, address minter)
+    constructor(address defaultAdmin)
         ERC20("RCUSD", "rcUSD")
         ERC20Permit("RCUSD")
     {
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function addMinter(address newMinter) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(MINTER_ROLE, newMinter);
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
