6664cb77df822f0a55d3f17f68adf67e659b63c1
Nandy Bâ <nandyba@yahoo.fr>
2024-04-01 14:50:50 -0300

    test: vault create tests setup

---
+++ test/Vault.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/Vault.sol";
+import "../src/RCUSD.sol";
+import "../src/MockRealTProperty.sol";
+
+contract VaultTest is Test {
+    
+    PersonalVault vault;
+    RCUSD rcUSD;
+    RealTProperty monicaPropertyToken;
+    address admin = address(1);
+    address deployer = address(2);
+    address user = address(3);
+    
+
+    function setUp() public {
+        rcUSD = new RCUSD(admin);
+        monicaPropertyToken = new RealTProperty("Monica", "Monica", 0);
+
+        
+
+
+        vm.startPrank(deployer);
+        vault = new PersonalVault(address(monicaPropertyToken), address(rcUSD));
+
+        vm.stopPrank();
+    }
+
+    
+
+}
