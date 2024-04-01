d0921de1b9100257642ca72544cc836c968205fc
Nandy Bâ <nandyba@yahoo.fr>
2024-04-01 15:02:06 -0300

    test: deposit (#13)
    * test: vault create tests setup
    
    * test: update setup
    
    * test: add deposit test
    
    * test: add check condition for deposit test

---
+++ test/Vault.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/Vault.sol";
+import "../src/RCUSD.sol";
+import "../src/MockRealTProperty.sol";
+
+contract VaultTest is Test {
+    
+    PersonalVault vault;
+    RCUSD rcUSD;
+    RealTProperty monicaPropertyToken;
+    address admin = address(1);
+    address deployer = address(2);
+    address user = address(3);
+    
+
+    function setUp() public {
+        rcUSD = new RCUSD(admin);
+        
+        vm.startPrank(deployer);
+        monicaPropertyToken = new RealTProperty("Monica", "Monica", 0);
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        vault = new PersonalVault(address(monicaPropertyToken), address(rcUSD));
+
+        vm.stopPrank();
+    }
+
+    function testDeposit() public {
+        uint256 supplyAmount = 10e18;
+        monicaPropertyToken.mint(user, supplyAmount);
+
+
+        assertEq(vault.realTTokenBalance(), 0);
+        assertEq(monicaPropertyToken.balanceOf(user), supplyAmount);
+        
+        vm.startPrank(user);
+        monicaPropertyToken.approve(address(vault), supplyAmount);
+        vault.deposit(supplyAmount);
+        vm.stopPrank();
+
+        assertEq(vault.realTTokenBalance(), supplyAmount);
+        assertEq(monicaPropertyToken.balanceOf(user), 0);
+    }
+
+    
+
+}
